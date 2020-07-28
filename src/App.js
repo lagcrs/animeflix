d3de45004a5c68a3b719083057d8e457c784d9fb
Larissa Silva <larissa.gcrs@gmail.com>
2020-07-28 02:36:22 -0300

    Fazendo Carousel

--- src/App.js
+++ src/App.js
@@ -2,18 +2,62 @@ import React from 'react';
 import Menu from './components/Menu';
 import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div style={{ background: "#141414" }}>
+		<Menu />
+
 		<BannerMain
 			videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
 			url={dadosIniciais.categorias[0].videos[0].url}
 			videoDescription={"Naruto é uma série de mangá escrita  por Masashi Kishimoto, que conta a história de Naruto Uzumaki, um jovem ninja que deseja reconhecimento e sonha em se tornar Hokage, o ninja líder de sua vila."}
 		/>
-      	<Menu />
-	  	<Footer />
+
+		<Carousel
+			ignoreFirstVideo
+			category={dadosIniciais.categorias[0]}
+		/>
+
+		<Carousel
+			category={dadosIniciais.categorias[1]}
+		/>
+
+		<Carousel
+			category={dadosIniciais.categorias[2]}
+		/>      
+
+		<Carousel
+			category={dadosIniciais.categorias[3]}
+		/>      
+
+		<Carousel
+			category={dadosIniciais.categorias[4]}
+		/>      
+
+		<Carousel
+			category={dadosIniciais.categorias[5]}
+		/> 
+
+		<Carousel
+			category={dadosIniciais.categorias[6]}
+		/> 
+
+		<Carousel
+        	category={dadosIniciais.categorias[7]}
+      	/>  
+
+		<Carousel
+        	category={dadosIniciais.categorias[8]}
+      	/>  
+
+		<Carousel
+        	category={dadosIniciais.categorias[9]}
+      	/> 
+
+	<Footer />
 
     </div>
   );
