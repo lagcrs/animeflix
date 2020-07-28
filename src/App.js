6b151ca85c17db96816c145663f01b0b53ed29e3
Larissa Silva <larissa.gcrs@gmail.com>
2020-07-28 02:14:57 -0300

    Banner Main finalizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
 import BannerMain from './components/BannerMain';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div style={{ background: "#141414" }}>
-		<BannerMain/>
+		<BannerMain
+			videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+			url={dadosIniciais.categorias[0].videos[0].url}
+			videoDescription={"Naruto é uma série de mangá escrita  por Masashi Kishimoto, que conta a história de Naruto Uzumaki, um jovem ninja que deseja reconhecimento e sonha em se tornar Hokage, o ninja líder de sua vila."}
+		/>
       	<Menu />
 	  	<Footer />
 
--- src/components/BannerMain/index.js
+++ src/components/BannerMain/index.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import VideoIframeResponsive from './components/VideoIframeResponsive';
+import { BannerMainContainer, ContentAreaContainer, WatchButton } from './styles';
 
-export default function BannerMain({}) {
-    
+function getYouTubeId(youtubeURL) {
+    return youtubeURL
+        .replace(
+        /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+        '$7',
+        );
+}
+
+export default function BannerMain({ videoTitle, videoDescription, url }) {
+    const youtubeID = getYouTubeId(url);
+    const bgURL = `https://img.youtube.com/vi/${youtubeID}/maxresdefault.jpg`;
+
+    return (
+        <BannerMainContainer backgroundImage={bgURL}>
+            <ContentAreaContainer>
+                <ContentAreaContainer.Item>
+                    <ContentAreaContainer.Title>
+                        {videoTitle}
+                    </ContentAreaContainer.Title>
+
+                    <ContentAreaContainer.Description>
+                        {videoDescription}
+                    </ContentAreaContainer.Description>
+                </ContentAreaContainer.Item>
+
+                <ContentAreaContainer.Item>
+                    <VideoIframeResponsive  youtubeID={youtubeID}/>
+                    <WatchButton>Assistir</WatchButton>
+                </ContentAreaContainer.Item>
+            </ContentAreaContainer>
+
+        </BannerMainContainer>
+    )
 }
