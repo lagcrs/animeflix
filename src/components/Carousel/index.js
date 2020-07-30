b9d1531ad3b54b48425f5984df2d6beda7916f4a
Larissa Silva <larissa.gcrs@gmail.com>
2020-07-30 00:43:32 -0300

    Carousel da Home

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink, VideoCardTitle } from './styles';
 import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
 
-function VideoCardGroup({
+function Carousel({
   ignoreFirstVideo,
   category,
 }) {
@@ -25,26 +26,26 @@ function VideoCardGroup({
           }
         </>
       )}
-      <VideoCardList>
+      <Slider>
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
           }
 
           return (
-            <li key={video.titulo}>
+            <SliderItem key={video.titulo}>
               <VideoCard
                 videoTitle={video.titulo}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
-			  <VideoCardTitle>{video.titulo}</VideoCardTitle>
-            </li>
+			        <VideoCardTitle>{video.titulo}</VideoCardTitle>
+            </SliderItem>
           );
         })}
-      </VideoCardList>
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
 
-export default VideoCardGroup;
+export default Carousel;
